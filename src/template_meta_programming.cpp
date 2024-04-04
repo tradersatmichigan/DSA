bbdd01e7129a3558154c621b67836321c87c03eb
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2024-04-04 15:13:37 -0400

    more comments

--- src/template_meta_programming.cpp
+++ src/template_meta_programming.cpp
@@ -12,5 +12,9 @@ struct factorial<0>
 
 int main() {
   // 150 factorial is calculated at compile time instead of runtime
+  // We can use template meta programing to calculate a lot of things
+  // at compile time but it will be very gross and unreadable.
+  // In modern cpp, we avoid TMP because it isnt necessary
+  // with the addition of constexpr and consteval
   std::cout << "150 factorial is : " << factorial<150>::value << '\n';
 }
