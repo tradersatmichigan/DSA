d8151a63b5e70e3ecc1f41979e278ec1f59d28a5
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2024-04-24 01:49:36 -0400

    implement segment tree

--- src/segment_tree.cpp
+++ src/segment_tree.cpp
@@ -1,46 +1,72 @@
+#include <cmath>
 #include <iostream>
+#include <numeric>
 #include <vector>
+using std::vector;
+
+/*
+* Return the next power of 2 that is at least as great as the input
+*/
+int nextPower2(int size) {
+  int power = 1;
+  while (power < size)
+    power <<= 1;
+  return power;
+}
 
 class segmentTree {
-  std::vector<int> data;
+ vector<int> tree;
   const int n;
 
+  int _query(int root, int start, int end, int queryStart, int queryEnd) const {
+    if (start > queryEnd || end < queryStart) return 0;
+    else if (start >= queryStart && end <= queryEnd) return tree[root]; 
+    int elementsPerChild = (end - start) / 2;
+    return _query(root * 2, start, start + elementsPerChild, queryStart, queryEnd) + 
+           _query(root * 2 + 1, end - elementsPerChild, end, queryStart, queryEnd);
+  }
 public:
-  segmentTree(std::vector<int>& v) : n(v.size()) {
-    // need two n for binary tree of diameter n
-    data.resize(n * 2);
-   
-    for (int i = 0; i < n; ++i) {
-      data[n + i] = v[i];
-    }
+  segmentTree(vector<int>& data) : n(data.size()) {
+    int width = nextPower2(data.size());
+    int size = 2 * width;
+    tree.resize(size);
 
-    for (int i = n - 1; i >= 0; --i) {
-      data[i] = data[i * 2 + 1];
-      if (i * 2 + 2 < 2 * n) data[i] += data[i * 2 + 2];
+    // load in the base data
+    auto treePtr = tree.begin() + width;
+    for (auto dataPtr = data.begin(); dataPtr != data.end(); ++dataPtr, ++treePtr) {
+      *treePtr = *dataPtr;
     }
-
+    // consolidate the numbers into sums
+    for (int node = tree.size() / 2; node > 0; --node) {
+      tree[node] = tree[2 * node] + tree[2 * node + 1];
+    }
   }
 
-  int mid (int l, int r) const { return l + (r - l) / 2; }
-
-  int query(int l, int r, int i = 0, int s = 0, int e = -1) {
-    if (e == -1) e = n - 1;
-    if (s >= l && r >= e) {
-      printf("USING SEG: %d to %d = %d\n", s, e, data[i]);
-      return data[i];
-    } // if the whole interval in contained
-    if (s > r || e < l) return 0;
-    int midp = mid(s, e);
-    return query(l, r, 2 * i + 1, s, midp) + 
-           query(l, r, 2 * i + 2, midp + 1, e);
+  int query(int l, int r) {
+    return _query(1, 0, tree.size()/2, l, r);
   }
-
 };
 
+void test(segmentTree & st, vector<int> &data, int l, int r) {
+  static int testNum = 1;
+  int actual = std::accumulate(data.begin() + l, data.begin() + r, 0);
+  int observed = st.query(l, r);
+  if (actual == observed) {
+    std::cout << "Test num " << testNum << " passed\n";
+  }
+  else {
+    std::cout << "FAILED TEST " << testNum << '\n'; 
+    std::cout << "EXPECTED: " << actual << " GOT: " << observed << '\n';
+  }
+  ++testNum;
+}
+
 int main() {
-  std::vector<int> data = {1, 2, 3, 4, 5};
+  vector<int> data = {0, 1, 2, 3, 4, 5, 6};
   segmentTree st(data);
-  std::cout << st.query(0, 4) << '\n';
-  std::cout << st.query(1, 4) << '\n';
-  std::cout << st.query(1, 3) << '\n';
+
+  test(st, data, 0, 7);
+  test(st, data, 3, 5);
+  test(st, data, 2, 6);
+  test(st, data, 1, 4);
 }
