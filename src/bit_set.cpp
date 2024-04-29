59262cf2b917e2a04b3a23da6d6a8d98356660eb
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2024-04-29 01:45:23 -0400

    comments

--- src/bit_set.cpp
+++ src/bit_set.cpp
@@ -1,9 +1,21 @@
-#include <iostream>
+#include <cassert>
+
+/*
+* A macro to yield the size of a type in bits 
+* Notice that sizeof returns the size in bytes
+*/
 #define bitSize(T) 8 * sizeof(T)
 
+/*
+  * bit_set is a highly memory efficient set implementation
+  * that uses one bit per potential key. Therefore, we can 
+  * represent a set of size 800 with only around 100 bytes.
+  */
 typedef unsigned int UI;
 template<UI size, typename T = unsigned int>
 class bit_set {
+  // create an array of size ceil(size / typesize)
+  // essentially using as few bits as possible
   T set[size / bitSize(T) + (bool) (size % bitSize(T))];
 
 public:
@@ -14,7 +26,7 @@ public:
     set[index] |= ((T)1 << offset);
   }
 
-  bool contains(unsigned int key) {
+  bool contains(UI key) {
     UI index = key / bitSize(T);
     UI offset = key % bitSize(T);
     return set[index] & ((T)1 << offset);
@@ -24,6 +36,14 @@ public:
 int main() {
   bit_set<1000> bs;
 
+  assert(!bs.contains(3));
+
   bs.add(3);
-  bs.contains(3);
+  assert(bs.contains(3));
+  assert(!bs.contains(4));
+
+  bs.add(977);
+  assert(bs.contains(977));
+
+
 }
