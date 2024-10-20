243f8d8c38a9a4dcf63d968be8a1a89971f51ea5
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2024-10-19 20:46:03 -0400

    knuth morris pratt

---
+++ src/kmp.cpp
@@ -0,0 +1,78 @@
+#include <algorithm>
+#include <iostream>
+#include <ostream>
+#include <vector>
+#include <string>
+
+std::vector<size_t> LPS(const std::string &s) {
+  assert(s.size());
+  std::vector<size_t> v(s.size());
+
+  v[0] = 0;
+  int len = 0;
+
+  for (int i = 1; i < s.size(); ++i) {
+    while (len > 0 && s[len] != s[i]) len = v[len - 1];
+
+    len += s[len] == s[i];
+
+    v[i] = len;
+  }
+
+  return v;
+}
+
+size_t find(const std::string &needle, const std::string &haystack) {
+  const auto v = LPS(needle);
+
+  size_t needle_ptr = 0;
+
+  for (size_t i = 0; i < haystack.size(); ++i) {
+
+    if (haystack[i] != needle[needle_ptr]) {
+      if (needle_ptr > 0)
+        needle_ptr = v[needle_ptr - 1];
+    }
+    else {
+      ++needle_ptr;
+    }
+
+    if (needle_ptr == needle.size()) {
+      return 1 + i - needle.size();
+    }
+  }
+
+  return haystack.size();
+}
+
+void test(size_t string_size) {
+  const auto gen = []() {
+    return (rand() % 26) + 'a';
+  };
+
+  std::string haystack(string_size, 0);
+  std::generate(haystack.begin(), haystack.end(), gen);
+
+  size_t search_start = rand() % haystack.size();
+  size_t search_size = rand() % (haystack.size() - search_start);
+  ++search_size;
+
+  std::string needle = haystack.substr(search_start, search_size);
+  
+  const auto actual = find(needle, haystack);
+  const auto expected = haystack.find(needle);
+
+  if (actual != expected) {
+    printf("FAILING TEST! %ld != %ld\n", actual, expected);
+  }
+}
+int main() {
+  const int size = 200;
+  const int num_test = 30;
+  srand(0);
+
+  for (int i = 0; i < num_test; ++i) {
+    test(size);
+    std::cout << "Pass test " << i << std::endl;
+  }
+}
